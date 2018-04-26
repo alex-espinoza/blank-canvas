ebced5744ed460fc3b0d860cc024b8b9dfd39c69
Alex Espinoza <aespinoza@aspria.net>
2018-04-25 23:06:35 -0400

    Javascripts now being babelified from es6 to 5, then minified

--- Gulpfile.js
+++ Gulpfile.js
@@ -5,9 +5,13 @@ var sass = require('gulp-sass');
 var postcss = require('gulp-postcss');
 var cssnext = require('postcss-cssnext');
 var cssnano = require('cssnano');
+var babel = require('gulp-babel');
+var uglify = require('gulp-uglify');
+var pump = require('pump');
 var browserSync = require('browser-sync').create();
+var gutil = require('gulp-util');
 
-gulp.task('serve', ['sass, javascripts'], function() {
+gulp.task('serve', ['sass', 'javascript'], function() {
   browserSync.init({
     server: {
       baseDir: './',
@@ -15,22 +19,41 @@ gulp.task('serve', ['sass, javascripts'], function() {
     }
   });
 
-  gulp.watch('source/sass/*.sass', ['sass']);
-  gulp.watch('source/js/*.js').on('change', browserSync.reload);
-  gulp.watch('source/*.html').on('change', browserSync.reload);
+  gulp.watch('src/sass/*.sass', ['sass']);
+  gulp.watch('src/js/*.js', ['javascript-reload']);
+  gulp.watch('src/*.html').on('change', browserSync.reload);
 });
 
 gulp.task('sass', function() {
   var plugins = [
     cssnext({browsers: ['> 1%']}),
-    cssnano()
+    cssnano(),
   ];
 
-  return gulp.src('source/sass/*.sass')
+  return gulp.src('src/sass/*.sass')
           .pipe(sass().on('error', sass.logError))
           .pipe(postcss(plugins))
-          .pipe(gulp.dest('./source/css/'))
-          .pipe(browserSync.stream());
+          .pipe(gulp.dest('./src/css/'))
+          .pipe(browserSync.stream())
+});
+
+gulp.task('javascript', function() {
+  var options = {
+    mangle: {
+      toplevel: true,
+    },
+  };
+
+  return gulp.src('src/js/*.js')
+          .pipe(babel({
+            presets: ['@babel/env']
+          }))
+          .pipe(uglify(options).on('error', function (err) { gutil.log(gutil.colors.red('[Error]'), err.toString()); }))
+          .pipe(gulp.dest('./src/scripts/'))
+});
+
+gulp.task('javascript-reload', ['javascript'], function() {
+  browserSync.reload();
 });
 
 gulp.task('default', ['serve']);
---
+++ src/js/script.js
@@ -0,0 +1,12 @@
+// hey this is a comment
+console.log('Testing');
+const cool = {
+  one: 'one',
+  two: 'two',
+  three: 'three'
+}
+let { one, two, three } = cool;
+console.log('nah');
+console.log(one);
+
+
